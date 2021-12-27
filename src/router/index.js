077a48964d82a5320d8ae402e7d5ce39f4db4ef0
Rodney Sostras <rodney.sostras@gmail.com>
2021-12-27 03:41:58 -0300

    basic improvement

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,23 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import HomeView from '../views/Home.vue';
+import HomeView from '@/views/home';
+import PokemonView from '@/views/pokemon';
+import SearchView from '@/views/search';
 
 const routes = [
     {
         path: '/',
-        name: 'Home',
+        name: 'home',
         component: HomeView,
+    },
+    {
+        path: '/pokemon/:identifier',
+        name: 'pokemon',
+        component: PokemonView,
+    },
+    {
+        path: '/search/:endpoint/:identifier',
+        name: 'search',
+        component: SearchView,
     },
 ];
 
