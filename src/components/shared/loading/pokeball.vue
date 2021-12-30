b27de67bd11ccc39eeda6adf2397977265f385c6
Rodney Sostras <rodney.sostras@gmail.com>
2021-12-30 03:13:55 -0300

    feat: loading in pokeball format

---
+++ src/components/shared/loading/pokeball.vue
@@ -0,0 +1,13 @@
+<template>
+    <svg viewBox="0 0 24 24" class="h-12 w-12 opacity-10 animate-spin">
+        <use href="@/assets/img/pokeball.svg#svg" width="100%" height="100%" />
+    </svg>
+</template>
+
+<script>
+export default {
+    name: 'LoadingPokeball',
+};
+</script>
+
+<style></style>
