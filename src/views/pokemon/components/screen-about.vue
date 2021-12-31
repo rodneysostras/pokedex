7c3fedde1070a28090a093ed4cbcc0da3bc8ad1f
Rodney Sostras <rodney.sostras@gmail.com>
2021-12-31 18:31:29 -0300

    feat: screen about and moves in pokemon

---
+++ src/views/pokemon/components/screen-about.vue
@@ -0,0 +1,21 @@
+<template>
+    <Container title="About" layout="about">
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum cupiditate facilis numquam iusto deserunt ut
+            maxime tempora cumque sequi repellendus ex accusantium, quo necessitatibus optio nulla error voluptas dolore
+            facere!
+        </p>
+    </Container>
+</template>
+
+<script>
+import Container from './container';
+export default {
+    name: 'ScreenAbout',
+    components: {
+        Container,
+    },
+};
+</script>
+
+<style></style>
---
+++ src/views/pokemon/components/screen-moves.vue
@@ -0,0 +1,38 @@
+<template>
+    <Container title="Moves" layout="move">
+        <ul class="relative flex flex-row justify-between flex-wrap pb-6">
+            <li class="w-1/2 text-center">Lorem ipsum dolor</li>
+            <li class="w-1/2 text-center">Lorem ipsum dolor</li>
+            <li class="w-1/2 text-center">Lorem ipsum dolor</li>
+            <li class="w-1/2 text-center">Lorem ipsum dolor</li>
+            <li class="w-1/2 text-center">Lorem ipsum dolor</li>
+            <li class="w-1/2 text-center">Lorem ipsum dolor</li>
+            <li class="absolute bottom-0 h-12 w-full font-bold text-center text-white rounded-b-xl show-more uppercase">
+                show more
+            </li>
+        </ul>
+    </Container>
+</template>
+
+<script>
+import Container from './container';
+export default {
+    name: 'ScreenMove',
+    components: {
+        Container,
+    },
+    props: {
+        dataset: {
+            type: Array,
+            requered: false,
+        },
+    },
+};
+</script>
+
+<style>
+.show-more {
+    line-height: 3rem;
+    background: linear-gradient(to top, var(--pokemon-color) 0%, var(--pokemon-color) 60%, rgba(96, 165, 250, 0) 100%);
+}
+</style>
