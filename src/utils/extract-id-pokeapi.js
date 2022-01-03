9c31faee6b3ac8a16a26e575e9e971c25ce2ea7d
Rodney Sostras <rodney.sostras@gmail.com>
2022-01-02 22:12:42 -0300

    fix: regex extracting content correctly

--- src/utils/extract-id-pokeapi.js
+++ src/utils/extract-id-pokeapi.js
@@ -1,5 +1,5 @@
 export function extractIdPokemonFromUrlPokeApi(url) {
-    return /\/([\d]+)\//gm.exec(url)[1] || 0;
+    return /\/([\d]*)\/$/gm.exec(url)[1] || 0;
 }
 
 export default extractIdPokemonFromUrlPokeApi;
